2496087c197929336325750304fe632ac0f15060
Sergey Tarasov <dzirtusss@gmail.com>
2023-01-05 11:12:40 +0200

    Add api token validation

--- lib/core/controlplane_api_direct.rb
+++ lib/core/controlplane_api_direct.rb
@@ -4,6 +4,11 @@ class ControlplaneApiDirect
   API_METHODS = { get: Net::HTTP::Get, post: Net::HTTP::Post, put: Net::HTTP::Put, delete: Net::HTTP::Delete }.freeze
   API_HOSTS = { api: "https://api.cpln.io", logs: "https://logs.cpln.io" }.freeze
 
+  API_TOKEN_REGEX = Regexp.union(
+    /^[\w.]{155}$/, # CPLN_TOKEN format
+    /^[\w\-._]{1134}$/ # 'cpln profile token' format
+  ).freeze
+
   def call(url, method:, host: :api) # rubocop:disable Metrics/MethodLength
     uri = URI("#{API_HOSTS[host]}#{url}")
     request = API_METHODS[method].new(uri)
@@ -25,6 +30,11 @@ class ControlplaneApiDirect
   end
 
   def api_token
-    @@api_token ||= ENV.fetch("CPLN_TOKEN", `cpln profile token`.chomp) # rubocop:disable Style/ClassVars
+    return @@api_token if defined?(@@api_token)
+
+    @@api_token = ENV.fetch("CPLN_TOKEN", `cpln profile token`.chomp) # rubocop:disable Style/ClassVars
+    return @@api_token if @@api_token.match?(API_TOKEN_REGEX)
+
+    abort("ERROR: Unknown API token format. Please re-run 'cpln profile login' or set correct CPLN_TOKEN env variable")
   end
 end
