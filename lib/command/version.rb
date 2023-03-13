6c05ce03020b5762a12cbd4a6a6498981c365eca
Rafael Gomes <rafael@shakacode.com>
2023-03-13 16:37:21 -0300

    Add version command

---
+++ lib/command/version.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Command
+  class Version < Base
+    NAME = "version"
+    DESCRIPTION = "Displays the current version of the CLI"
+    LONG_DESCRIPTION = <<~HEREDOC
+      - Displays the current version of the CLI
+      - Can also be done with `cpl --version` or `cpl -v`
+    HEREDOC
+
+    def call
+      puts Cpl::VERSION
+    end
+  end
+end
