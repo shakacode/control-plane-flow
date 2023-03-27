7eb8a61666e945389b547a265fcefd9dd551859c
Rafael Gomes <rafael@shakacode.com>
2023-03-27 13:57:30 -0300

    Add tmp_stderr to Shell

--- lib/core/shell.rb
+++ lib/core/shell.rb
@@ -1,12 +1,34 @@
 # frozen_string_literal: true
 
 class Shell
+  class << self
+    attr_reader :tmp_stderr
+  end
+
   def self.shell
     @shell ||= Thor::Shell::Color.new
   end
 
   def self.stderr
     @stderr ||= $stderr
+  end
+
+  def self.use_tmp_stderr
+    @tmp_stderr = Tempfile.create
+
+    yield
+
+    @tmp_stderr.close
+    @tmp_stderr = nil
+  end
+
+  def self.write_to_tmp_stderr(message)
+    tmp_stderr.write(message)
+  end
+
+  def self.read_from_tmp_stderr
+    tmp_stderr.rewind
+    tmp_stderr.read.strip
   end
 
   def self.color(message, color_key)
