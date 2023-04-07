c6b626c2639195d5fcead4f48e2bbd4fab0416c7
Rafael Gomes <rafael@shakacode.com>
2023-04-06 22:29:56 -0300

    Include deployed endpoints in output for deploy-image command (#35)

--- lib/command/deploy_image.rb
+++ lib/command/deploy_image.rb
@@ -11,17 +11,27 @@ module Command
       - Deploys the latest image to app workloads
     DESC
 
-    def call
+    def call # rubocop:disable Metrics/MethodLength
+      deployed_endpoints = {}
+
       image = latest_image
 
       config[:app_workloads].each do |workload|
-        cp.fetch_workload!(workload).dig("spec", "containers").each do |container|
+        workload_data = cp.fetch_workload!(workload)
+        workload_data.dig("spec", "containers").each do |container|
           next unless container["image"].match?(%r{^/org/#{config.org}/image/#{config.app}:})
 
-          step("Deploying image '#{image}' for workload '#{container['name']}'") do
-            cp.workload_set_image_ref(workload, container: container["name"], image: image)
+          container_name = container["name"]
+          step("Deploying image '#{image}' for workload '#{container_name}'") do
+            cp.workload_set_image_ref(workload, container: container_name, image: image)
+            deployed_endpoints[container_name] = workload_data.dig("status", "endpoint")
           end
         end
+      end
+
+      progress.puts("\nDeployed endpoints:")
+      deployed_endpoints.each do |workload, endpoint|
+        progress.puts("  - #{workload}: #{endpoint}")
       end
     end
   end
