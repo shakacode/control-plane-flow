a44219e899a8f91c19b0a0d1779bffde41441492
Rafael Gomes <rafael@shakacode.com>
2023-09-20 12:19:01 -0300

    test: add partial specs for ControlplaneApiDirect

---
+++ spec/core/controlplane_api_direct_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe ControlplaneApiDirect do
+  let(:described_instance) { described_class.new }
+
+  describe "#api_host" do
+    it "returns correct host for 'api' when CPLN_ENDPOINT is not set" do
+      allow(ENV).to receive(:fetch).with("CPLN_ENDPOINT", "https://api.cpln.io").and_return("https://api.cpln.io")
+
+      host = described_instance.api_host(:api)
+
+      expect(host).to eq("https://api.cpln.io")
+    end
+
+    it "returns correct host for 'api' when CPLN_ENDPOINT is set" do
+      allow(ENV).to receive(:fetch).with("CPLN_ENDPOINT", "https://api.cpln.io").and_return("http://api.cpln.io")
+
+      host = described_instance.api_host(:api)
+
+      expect(host).to eq("http://api.cpln.io")
+    end
+
+    it "returns correct host for 'logs'" do
+      host = described_instance.api_host(:logs)
+
+      expect(host).to eq("https://logs.cpln.io")
+    end
+  end
+
+  describe "#api_token" do
+    before do
+      described_class.remove_class_variable(:@@api_token) if described_class.class_variable_defined?(:@@api_token)
+    end
+
+    it "returns token from CPLN_TOKEN" do
+      allow(ENV).to receive(:fetch).with("CPLN_TOKEN", nil).and_return("token_1")
+
+      token = described_instance.api_token
+
+      expect(token).to eq("token_1")
+    end
+
+    it "returns token from 'cpln profile token'" do
+      allow(ENV).to receive(:fetch).with("CPLN_TOKEN", nil).and_return(nil)
+      allow(described_instance).to receive(:`).with("cpln profile token").and_return("token_2")
+
+      token = described_instance.api_token
+
+      expect(token).to eq("token_2")
+    end
+
+    it "raises error if token is not found" do
+      allow(ENV).to receive(:fetch).with("CPLN_TOKEN", nil).and_return(nil)
+      allow(described_instance).to receive(:`).with("cpln profile token").and_return("")
+
+      message = "Unknown API token format. " \
+                "Please re-run 'cpln profile login' or set the correct CPLN_TOKEN env variable."
+      expect do
+        described_instance.api_token
+      end.to raise_error(RuntimeError, message)
+    end
+  end
+end
