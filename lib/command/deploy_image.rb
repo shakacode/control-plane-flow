3d63c574cebce97fcb8464691845c4d28a7405d0
Mostafa Ahangarha <ahangarha@riseup.net>
2023-11-10 17:31:02 +0330

    Add dry-run to deploy-image

--- lib/command/deploy_image.rb
+++ lib/command/deploy_image.rb
@@ -17,13 +17,16 @@ module Command
       image = latest_image
 
       config[:app_workloads].each do |workload|
-        workload_data = cp.fetch_workload!(workload)
+        workload_data = dry_run? ? workload_date_for_dry_run : cp.fetch_workload!(workload)
+
         workload_data.dig("spec", "containers").each do |container|
           next unless container["image"].match?(%r{^/org/#{config.org}/image/#{config.app}:})
 
           container_name = container["name"]
+
+          show_dry_run_message("ControlPlane - workload set image ref") if dry_run?
           step("Deploying image '#{image}' for workload '#{container_name}'") do
-            cp.workload_set_image_ref(workload, container: container_name, image: image)
+            cp.workload_set_image_ref(workload, container: container_name, image: image) unless dry_run?
             deployed_endpoints[container_name] = workload_data.dig("status", "endpoint")
           end
         end
@@ -33,6 +36,24 @@ module Command
       deployed_endpoints.each do |workload, endpoint|
         progress.puts("  - #{workload}: #{endpoint}")
       end
+    end
+
+    private
+
+    def workload_date_for_dry_run
+      {
+        "spec" => {
+          "containers" => [
+            {
+              "name" => "DRY-WORKLOAD",
+              "image" => "/org/#{config.org}/image/#{config.app}:12345"
+            }
+          ]
+        },
+        "status" => {
+          "endpoint" => "DRY-ENDPOINT"
+        }
+      }
     end
   end
 end
