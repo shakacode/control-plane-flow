e1642eb51d2f0fd6df1d10c4ffcfdf076fc2ca59
Mostafa Ahangarha <ahangarha@riseup.net>
2023-11-30 19:35:55 +0330

    Change test to run Cpl::Cli

--- spec/command/generate_spec.rb
+++ spec/command/generate_spec.rb
@@ -2,7 +2,6 @@
 
 require "spec_helper"
 require "pathname"
-require "rake"
 
 GEM_ROOT_PATH = Pathname.new(Dir.pwd)
 GEM_TEMP_PATH = GEM_ROOT_PATH.join("tmp")
@@ -10,8 +9,12 @@ GENERATOR_PLAYGROUND_PATH = GEM_TEMP_PATH.join("sample-project")
 CONTROLPLANE_CONFIG_DIR_PATH = GENERATOR_PLAYGROUND_PATH.join(".controlplane")
 CPL_EXECUTABLE_PATH = GEM_ROOT_PATH.join("bin", "cpl")
 
-def sh_in_dir(dir, *shell_commands)
-  shell_commands.flatten.each { |shell_command| RakeFileUtils.sh %(cd '#{dir}' && #{shell_command.strip}) }
+def inside_dir(path)
+  original_working_dir = Dir.pwd
+  Dir.chdir path
+  yield if block_given?
+ensure
+  Dir.chdir original_working_dir
 end
 
 describe Command::Generate do
@@ -26,11 +29,11 @@ describe Command::Generate do
 
   context "when no configuration exist in the project" do
     it "generates base config files" do
-      command = "#{CPL_EXECUTABLE_PATH} generate"
-      sh_in_dir(GENERATOR_PLAYGROUND_PATH, command)
-
-      controlplane_config_file_path = CONTROLPLANE_CONFIG_DIR_PATH.join("controlplane.yml")
-      expect(controlplane_config_file_path).to exist
+      inside_dir(GENERATOR_PLAYGROUND_PATH) do
+        Cpl::Cli.start([described_class::NAME])
+        controlplane_config_file_path = CONTROLPLANE_CONFIG_DIR_PATH.join("controlplane.yml")
+        expect(controlplane_config_file_path).to exist
+      end
     end
   end
 end
