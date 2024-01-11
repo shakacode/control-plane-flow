587ef7881d2ae3ce231deb3ff48b4bd724aaf3ce
Rafael Gomes <rafael@shakacode.com>
2024-01-11 19:30:36 -0300

    test: add specs for env command with spec/dummy app

---
+++ spec/command/env_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Command::Env do
+  context "when app does not exist" do
+    let!(:app) { dummy_test_app }
+
+    it "raises error", :fast do
+      result = run_cpl_command("env", "-a", app)
+
+      expect(result[:status]).to eq(1)
+      expect(result[:stderr]).to include("Can't find app '#{app}'")
+    end
+  end
+
+  context "when app exists" do
+    let!(:app) { dummy_test_app("default", create_if_not_exists: true) }
+
+    it "displays app-specific environment variables", :fast do
+      result = run_cpl_command("env", "-a", app)
+
+      expect(result[:status]).to eq(0)
+      expect(result[:stdout]).to include("REDIS_URL=redis://redis.#{app}.cpln.local:6379")
+      expect(result[:stdout])
+        .to include("DATABASE_URL=postgres://postgres:password@postgres.#{app}.cpln.local:5432/#{app}")
+    end
+  end
+end
