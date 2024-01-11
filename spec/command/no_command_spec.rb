ac9ade876f0686d90f82d822615e30e5c71dad35
Rafael Gomes <rafael@shakacode.com>
2024-01-11 19:45:15 -0300

    test: add specs for no command with spec/dummy app

---
+++ spec/command/no_command_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Command::NoCommand do
+  context "when called with nothing" do
+    it "displays help", :fast do
+      result = run_cpl_command
+
+      expect(result[:status]).to eq(0)
+      expect(result[:stdout]).to include("cpl commands")
+    end
+  end
+
+  context "when called with --version" do
+    it "displays version, :fast" do
+      result = run_cpl_command("--version")
+
+      expect(result[:status]).to eq(0)
+      expect(result[:stdout]).to include(Cpl::VERSION)
+    end
+  end
+end
