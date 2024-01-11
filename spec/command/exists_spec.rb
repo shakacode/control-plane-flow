d93eba36ced8d148be9736667aa629be8e49eac4
Rafael Gomes <rafael@shakacode.com>
2024-01-11 19:33:14 -0300

    test: add specs for exists command with spec/dummy app

---
+++ spec/command/exists_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Command::Exists do
+  context "when app does not exist" do
+    let!(:app) { dummy_test_app }
+
+    it "exits with 1", :fast do
+      result = run_cpl_command("exists", "-a", app)
+
+      expect(result[:status]).to eq(1)
+    end
+  end
+
+  context "when app exists" do
+    let!(:app) { dummy_test_app("default", create_if_not_exists: true) }
+
+    it "exits with 0", :fast do
+      result = run_cpl_command("exists", "-a", app)
+
+      expect(result[:status]).to eq(0)
+    end
+  end
+end
