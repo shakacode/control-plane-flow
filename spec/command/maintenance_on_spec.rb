8fc79f70c4768681ea503ac991289ac511f0f95c
Rafael Gomes <rafael@shakacode.com>
2024-01-12 16:34:08 -0300

    test: add specs for maintenance:on command with spec/dummy app

--- spec/command/maintenance_on_spec.rb
+++ spec/command/maintenance_on_spec.rb
@@ -3,73 +3,51 @@
 require "spec_helper"
 
 describe Command::MaintenanceOn do
-  # rubocop:disable RSpec/AnyInstance
-  before do
-    allow(ENV).to receive(:fetch).with("CPLN_ENDPOINT", "https://api.cpln.io").and_return("https://api.cpln.io")
-    allow(ENV).to receive(:fetch).with("CPLN_TOKEN", nil).and_return("token")
-    allow(ENV).to receive(:fetch).with("CPLN_ORG", nil).and_return(nil)
-    allow(ENV).to receive(:fetch).with("CPLN_APP", nil).and_return(nil)
-    allow_any_instance_of(Config).to receive(:config_file_path).and_return("spec/fixtures/config.yml")
-    allow(Kernel).to receive(:sleep).and_return(true)
-  end
-  # rubocop:enable RSpec/AnyInstance
+  context "when app has no domain" do
+    let!(:app) { dummy_test_app("with-nothing") }
 
-  it "displays error if domain is not found", vcr: true do
-    allow(Shell).to receive(:abort)
-      .with("Can't find domain. " \
-            "Maintenance mode is only supported for domains that use path based routing mode " \
-            "and have a route configured for the prefix '/' on either port 80 or 443.")
+    it "raises error", :fast do
+      result = run_cpl_command("maintenance:on", "-a", app)
 
-    args = ["-a", "my-app-staging"]
-    run_command(described_class::NAME, *args)
-
-    expect(Shell).to have_received(:abort).once
+      expect(result[:status]).to eq(1)
+      expect(result[:stderr]).to include("Can't find domain")
+    end
   end
 
-  it "displays error if maintenance workload is not found", vcr: true do
-    allow(Shell).to receive(:abort)
-      .with("Can't find workload 'maintenance', " \
-            "please create it with 'cpl apply-template maintenance -a my-app-staging'.")
+  context "when maintenance workload does not exist" do
+    let!(:app) { dummy_test_app("default", create_if_not_exists: true) }
 
-    args = ["-a", "my-app-staging"]
-    run_command(described_class::NAME, *args)
+    it "raises error", :fast do
+      result = run_cpl_command("maintenance:on", "-a", app)
 
-    expect(Shell).to have_received(:abort).once
+      expect(result[:status]).to eq(1)
+      expect(result[:stderr]).to include("Can't find workload 'maintenance'")
+    end
   end
 
-  it "does nothing if maintenance mode is already enabled", vcr: true do
-    expected_output = <<~OUTPUT
-      Maintenance mode is already enabled for app 'my-app-staging'.
-    OUTPUT
+  context "when maintenance workload exists" do
+    let!(:app) { dummy_test_app("full", create_if_not_exists: true) }
 
-    args = ["-a", "my-app-staging"]
-    result = run_command(described_class::NAME, *args)
+    before do
+      allow(Kernel).to receive(:sleep)
 
-    expect(result[:stderr]).to eq(expected_output)
-  end
+      run_cpl_command!("ps:start", "-a", app, "--wait")
+    end
 
-  it "enables maintenance mode", vcr: true do
-    expected_output = <<~OUTPUT
-      Starting workload 'maintenance'... done!
+    it "does nothing if maintenance mode is already enabled", :slow do
+      run_cpl_command!("maintenance:on", "-a", app)
+      result = run_cpl_command("maintenance:on", "-a", app)
 
-      Waiting for workload 'maintenance' to be ready... done!
+      expect(result[:status]).to eq(0)
+      expect(result[:stderr]).to include("Maintenance mode is already enabled for app '#{app}'")
+    end
 
-      Switching workload for domain 'my-app-staging.example.com' to 'maintenance'... done!
+    it "enables maintenance mode", :slow do
+      run_cpl_command!("maintenance:off", "-a", app)
+      result = run_cpl_command("maintenance:on", "-a", app)
 
-      Stopping workload 'rails'... done!
-      Stopping workload 'redis'... done!
-      Stopping workload 'postgres'... done!
-
-      Waiting for workload 'rails' to not be ready... done!
-      Waiting for workload 'redis' to not be ready... done!
-      Waiting for workload 'postgres' to not be ready... done!
-
-      Maintenance mode enabled for app 'my-app-staging'.
-    OUTPUT
-
-    args = ["-a", "my-app-staging"]
-    result = run_command(described_class::NAME, *args)
-
-    expect(result[:stderr]).to eq(expected_output)
+      expect(result[:status]).to eq(0)
+      expect(result[:stderr]).to include("Maintenance mode enabled for app '#{app}'")
+    end
   end
 end
