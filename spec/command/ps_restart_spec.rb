215536afd82973311c607ce78db506ec60ba96da
Rafael Gomes <rafael@shakacode.com>
2024-01-11 21:43:31 -0300

    test: add specs for ps:restart command with spec/dummy app

---
+++ spec/command/ps_restart_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Command::PsRestart do
+  context "when any workload does not exist" do
+    let!(:app) { dummy_test_app("default", create_if_not_exists: true) }
+
+    it "raises error", :fast do
+      result = run_cpl_command("ps:restart", "-a", app)
+
+      expect(result[:status]).to eq(1)
+      expect(result[:stderr]).to include("Can't find workload 'rails'")
+    end
+  end
+
+  context "when all workloads exist" do
+    let!(:app) { dummy_test_app("full", create_if_not_exists: true) }
+
+    before do
+      run_cpl_command!("ps:start", "-a", app, "--wait")
+    end
+
+    it "restarts all workloads", :slow do
+      result = run_cpl_command("ps:restart", "-a", app)
+
+      expect(result[:status]).to eq(0)
+      expect(result[:stderr]).to match(/Restarting workload 'rails'[.]+? done!/)
+      expect(result[:stderr]).to match(/Restarting workload 'redis'[.]+? done!/)
+      expect(result[:stderr]).to match(/Restarting workload 'postgres'[.]+? done!/)
+    end
+
+    it "restarts specific workload", :slow do
+      result = run_cpl_command("ps:restart", "-a", app, "--workload", "rails")
+
+      expect(result[:status]).to eq(0)
+      expect(result[:stderr]).to match(/Restarting workload 'rails'[.]+? done!/)
+      expect(result[:stderr]).not_to include("redis")
+      expect(result[:stderr]).not_to include("postgres")
+    end
+  end
+end
