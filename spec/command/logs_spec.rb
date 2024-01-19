91c51e676808a31b2d82137a0b978ad5c08e6222
Rafael Gomes <rafael@shakacode.com>
2024-01-19 16:52:28 -0300

    test: add specs for logs command with spec/dummy app

---
+++ spec/command/logs_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Command::Logs do
+  let!(:app) { dummy_test_app("full", create_if_not_exists: true) }
+
+  before do
+    run_cpl_command!("ps:start", "-a", app, "--wait")
+  end
+
+  context "when no workload is provided" do
+    it "displays logs for one-off workload", :slow do
+      result = nil
+      expected_regex = /Rails .+? application starting in production/
+
+      spawn_cpl_command("logs", "-a", app) do |it|
+        result = it.wait_for(expected_regex)
+        it.kill
+      end
+
+      expect(result).to match(expected_regex)
+    end
+  end
+
+  context "when workload is provided" do
+    it "displays logs for specific workload", :slow do
+      result = nil
+      expected_regex = /Server initialized/
+
+      spawn_cpl_command("logs", "-a", app, "--workload", "redis") do |it|
+        result = it.wait_for(expected_regex)
+        it.kill
+      end
+
+      expect(result).to match(expected_regex)
+    end
+  end
+end
