7123d97f804ef9abccc578e5e1fa65ce80b690f1
Justin Gordon <justin@shakacode.com>
2026-05-27 18:37:48 -1000

    Address Heroku mapping review comments

--- spec/readme_heroku_command_mapping_spec.rb
+++ spec/readme_heroku_command_mapping_spec.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
+require "spec_helper"
+
 RSpec.describe "README Heroku command mapping" do # rubocop:disable RSpec/DescribeClass
-  let(:readme) { File.read("README.md") }
-  let(:table) do
-    match = readme.match(
+  let(:readme) { File.read(File.expand_path("../README.md", __dir__)) }
+  let(:table_match) do
+    readme.match(
       /## Mapping of Heroku Commands to `cpflow` and `cpln`\n\n(?<table>(?:\|.*\n)+)/
     )
-
-    raise "Could not locate Heroku mapping table in README.md" unless match
-
-    match[:table]
   end
+  let(:table) { table_match[:table] }
 
   it "does not leave placeholder mappings for Heroku commands" do
+    expect(table_match).to be_truthy, "Could not locate Heroku mapping table in README.md"
+
     command_rows = table.lines.select { |line| line.start_with?("| [heroku ") }
 
     expect(command_rows).not_to be_empty
 
     placeholders = command_rows.select do |line|
+      # Column indices after split("|"): 0=empty, 1=Heroku command, 2=cpflow mapping, 3=empty.
       line.split("|").map(&:strip).fetch(2, "") == "?"
     end
 
