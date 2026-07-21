d897ade9116f5ac98bbd6b0a5bbc54862789865c
Raghav <raghapb34n@gmail.com>
2026-07-21 18:51:55 +0530

    test: add unit specs for String#unindent, #underscore, and #indent patches

--- spec/patches/string_spec.rb
+++ spec/patches/string_spec.rb
@@ -24,4 +24,36 @@ RSpec.describe String do
       end
     end
   end
+
+  describe "#unindent" do
+    it "strips leading indentation common to all lines" do
+      indented = "  first line\n    second line\n"
+
+      expect(indented.unindent).to eq("first line\n  second line\n")
+    end
+  end
+
+  describe "#underscore" do
+    it "converts camel case to underscore format" do
+      expect("ControlPlaneFlow".underscore).to eq("control_plane_flow")
+    end
+
+    it "converts double colons to forward slashes" do
+      expect("Cpflow::Cli".underscore).to eq("cpflow/cli")
+    end
+
+    it "handles hyphenated words" do
+      expect("control-plane".underscore).to eq("control_plane")
+    end
+  end
+
+  describe "#indent" do
+    it "indents lines by the specified amount" do
+      expect("hello\nworld".indent(2)).to eq("  hello\n  world")
+    end
+
+    it "uses custom indent character when provided" do
+      expect("hello".indent(1, "\t")).to eq("\thello")
+    end
+  end
 end
