5959fd98b0d195675735f6a8ec5405cf682a7c3f
Justin Gordon <justin@shakacode.com>
2026-09-01 17:51:32 -1000

    Cover remaining shell shebang forms

--- spec/script/check_shell_scripts_spec.rb
+++ spec/script/check_shell_scripts_spec.rb
@@ -14,11 +14,17 @@ RSpec.describe "script/check_shell_scripts" do # rubocop:disable RSpec/DescribeC
 
       File.write("#{repo}/aaa-missing", "not a shell script\n")
       File.chmod(0o755, "#{repo}/aaa-missing")
+      File.binwrite("#{repo}/aaa-non-shell-binary", "\0" * 65_536)
+      File.chmod(0o755, "#{repo}/aaa-non-shell-binary")
       File.write("#{repo}/aab-empty", "")
       File.chmod(0o755, "#{repo}/aab-empty")
       File.write("#{repo}/missing.sh", "#!/bin/sh\n")
-      File.write("#{repo}/zzz-runner", "#!/usr/bin/env bash\n")
+      File.write("#{repo}/zzz-runner", "#!/usr/bin/env bash -e\n")
       File.chmod(0o755, "#{repo}/zzz-runner")
+      File.write("#{repo}/zzx-spaced-env-runner", "#!/usr/bin/env  bash\n")
+      File.chmod(0o755, "#{repo}/zzx-spaced-env-runner")
+      File.write("#{repo}/zzy-tabbed-env-runner", "#!/usr/bin/env\tbash\n")
+      File.chmod(0o755, "#{repo}/zzy-tabbed-env-runner")
       File.write("#{repo}/zzzy-crlf-runner", "#!/usr/bin/env bash\r\n")
       File.chmod(0o755, "#{repo}/zzzy-crlf-runner")
       File.write("#{repo}/zzzz-env-runner", "#!/usr/bin/env -S -iva shell -uOLD -C/tmp -P/bin FOO='x y' bash -e\n")
@@ -35,6 +41,8 @@ RSpec.describe "script/check_shell_scripts" do # rubocop:disable RSpec/DescribeC
       File.chmod(0o755, "#{repo}/zzzz-env-nested-empty-runner")
       File.write("#{repo}/zzzz-env-nested-space-runner", "#!/usr/bin/env -S --split-string=\" \" bash -e\n")
       File.chmod(0o755, "#{repo}/zzzz-env-nested-space-runner")
+      File.write("#{repo}/zzzz-env-top-level-long-runner", "#!/usr/bin/env --split-string=bash -e\n")
+      File.chmod(0o755, "#{repo}/zzzz-env-top-level-long-runner")
       File.write("#{repo}/zzzz-invalid-env-runner", "#!/usr/bin/env -S FOO=bar -i bash -e\n")
       File.chmod(0o755, "#{repo}/zzzz-invalid-env-runner")
       File.write("#{repo}/zzzz-python-runner", "#!/usr/bin/env -S FOO='x bash y' python3\n")
@@ -48,10 +56,12 @@ RSpec.describe "script/check_shell_scripts" do # rubocop:disable RSpec/DescribeC
 
       system("git", "init", "--quiet", repo) || raise("git init failed")
       tracked_files = [
-        "README.md", "aaa-missing", "aab-empty", "missing.sh", "script/check_shell_scripts", "zzz-runner",
+        "README.md", "aaa-missing", "aaa-non-shell-binary", "aab-empty", "missing.sh",
+        "script/check_shell_scripts", "zzz-runner", "zzx-spaced-env-runner", "zzy-tabbed-env-runner",
         "zzzy-crlf-runner", "zzzz-env-attached-runner", "zzzz-env-dash-runner", "zzzz-env-runner",
         "zzzz-env-nested-empty-runner", "zzzz-env-nested-long-runner", "zzzz-env-nested-runner",
-        "zzzz-env-nested-space-runner", "zzzz-invalid-env-runner", "zzzz-python-runner"
+        "zzzz-env-nested-space-runner", "zzzz-env-top-level-long-runner", "zzzz-invalid-env-runner",
+        "zzzz-python-runner"
       ]
       system("git", "-C", repo, "add", "--", *tracked_files) ||
         raise("git add failed")
@@ -66,9 +76,10 @@ RSpec.describe "script/check_shell_scripts" do # rubocop:disable RSpec/DescribeC
       expect(status).to be_success, stderr
       expect(stdout.lines.map(&:chomp)).to eq(
         [
-          "--", "script/check_shell_scripts", "zzz-runner", "zzzy-crlf-runner", "zzzz-env-attached-runner",
-          "zzzz-env-dash-runner", "zzzz-env-nested-empty-runner", "zzzz-env-nested-long-runner",
-          "zzzz-env-nested-runner", "zzzz-env-nested-space-runner", "zzzz-env-runner"
+          "--", "script/check_shell_scripts", "zzx-spaced-env-runner", "zzy-tabbed-env-runner", "zzz-runner",
+          "zzzy-crlf-runner", "zzzz-env-attached-runner", "zzzz-env-dash-runner",
+          "zzzz-env-nested-empty-runner", "zzzz-env-nested-long-runner", "zzzz-env-nested-runner",
+          "zzzz-env-nested-space-runner", "zzzz-env-runner", "zzzz-env-top-level-long-runner"
         ]
       )
     end
